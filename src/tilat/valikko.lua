e614dbec0e6057c3a46822776c6e42b9667855b3
PPeltola <petrus.peltola@edu.hel.fi>
2014-05-20 03:04:50 +0300

    Päävalikko + säätöä
    Aika rumaa jälkeä

---
+++ src/tilat/valikko.lua
@@ -0,0 +1,69 @@
+--[[
+	Paavalikko
+--]]
+
+
+-- Luodaan valikkotila
+valikko = {}
+
+
+function valikko:init()
+
+	onkoPause = false
+
+	paavalikko = Menu.new()
+
+	paavalikko:addItem{
+		nimi = "Aloita peli",
+		toiminto = function()
+			-- Gamestate.switch( peli )
+			print("Peli alkaa")
+		end
+	}
+
+	paavalikko:addItem{
+		nimi = "Asetukset",
+		toiminto = function()
+			-- Gamestate.switch( asetukset, self.mista )
+			print("Asetukset")
+		end
+	}
+
+	paavalikko:addItem{
+		nimi = "Lopeta",
+		toiminto = function()
+			love.event.quit()
+		end
+	}
+
+end
+
+
+function valikko:enter( mista )
+
+	self.mista = mista
+
+end
+
+
+function valikko:update( dt )
+
+	paavalikko:update( dt )
+
+end
+
+
+function valikko:draw()
+
+	love.graphics.draw( kuvat[ "testi_tausta.png" ], 0, 0 )
+
+	paavalikko:draw( 100, 200 )
+
+end
+
+
+function valikko:keypressed( nappain )
+
+	paavalikko:keypressed( nappain )
+
+end
